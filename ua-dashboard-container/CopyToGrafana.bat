81e1488549802cac9946779d0590455624d4f86c
Christian Fjeld <christian_fjeld@hotmail.com>
2021-02-10 10:31:47 +0100

    renamed ua-dashboard

---
+++ ua-dashboard-container/CopyToGrafana.bat
@@ -0,0 +1,3 @@
+
+
+xcopy "dist\*.*" "C:\Program Files\GrafanaLabs\grafana\data\plugins\ua-dashboard-container" /E/H /Y
--- ua-dashboard/CopyToGrafana.bat
+++
@@ -1,3 +0,0 @@
-
-
-xcopy "dist\*.*" "C:\Program Files\GrafanaLabs\grafana\data\plugins\ua-dashboard" /E/H /Y
